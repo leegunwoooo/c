970fd5ee8fbb856653a0e747c7578ebc264eff47
leegunwoooo <leegunwoo0325@gmail.com>
2024-04-23 16:35:54 +0900

    집가고싶다

---
+++ 1088.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a=0;
+    int i;
+    scanf("%d", &a);
+    if(i=1 i<=a i++){
+        if(a%3==0){
+            continue;
+        }
+        printf("%d", i);
+    }
+    return 0;
+}
